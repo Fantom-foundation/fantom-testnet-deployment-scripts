e79b013db5e098640f031ebbe167c5f4865766f6
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-05-10 18:16:03 +1000

    rename lachesis -> dag1

--- scripts/testnet-master.bash
+++ scripts/testnet-master.bash
@@ -5,29 +5,29 @@ declare -r parent_dir="${DIR%/*}";
 
 declare -ar nodes=('172.31.6.65' '172.31.8.33' '172.31.5.183' '172.31.3.125' '172.31.1.84');
 
-declare -r go_lachesis_service_file='go-lachesis.service';
+declare -r go_lachesis_service_file='dag1.service';
 declare -r evm_service_file='go-evm.service'
 
 function deploy() {
   printf 'Deploying testnet%d [%s]:\n' "$1" "${nodes[$1]}";
 
   # Config
-  rsync -avz "$parent_dir"/scripts/testnet.bash testnet"$1":/home/ubuntu/go/src/github.com/Fantom-foundation/go-lachesis/scripts/;
-  rsync -avz "$parent_dir"/testnet/lachesis_data_dir testnet"$1":/mnt/data/ --rsync-path='sudo rsync';
+  rsync -avz "$parent_dir"/scripts/testnet.bash testnet"$1":/home/ubuntu/go/src/github.com/SamuelMarks/dag1/scripts/;
+  rsync -avz "$parent_dir"/testnet/dag1_data_dir testnet"$1":/mnt/data/ --rsync-path='sudo rsync';
   ssh testnet"$1" "mkdir -p ~/.evm/eth" | sed "s/^/[${nodes[$1]}] /";
   rsync -avz "$parent_dir"/testnet/genesis.json testnet"$1":/home/ubuntu/.evm/eth/;
 
   # Lachesis
-  declare -r go_lachesis='go-lachesis';
-  declare -r lachesis_dir='/home/ubuntu/go/src/github.com/Fantom-foundation/'"$go_lachesis";
-  env -i PATH="$PATH" DATAL_DIR='/mnt/data' BUILD_DIR="$lachesis_dir/build" NODE="$1" NODE_ADDR="${nodes[$1]}" envsubst < "$parent_dir"/go-lachesis.tpl.service > "$parent_dir"/"$go_lachesis_service_file"."$1";
+  declare -r go_lachesis='dag1';
+  declare -r lachesis_dir='/home/ubuntu/go/src/github.com/SamuelMarks/'"$go_lachesis";
+  env -i PATH="$PATH" DATAL_DIR='/mnt/data' BUILD_DIR="$lachesis_dir/build" NODE="$1" NODE_ADDR="${nodes[$1]}" envsubst < "$parent_dir"/dag1.tpl.service > "$parent_dir"/"$go_lachesis_service_file"."$1";
   rsync -avz "$parent_dir"/"$go_lachesis_service_file"."$1" testnet"$1":/mnt/data/"$go_lachesis_service_file";
   ssh testnet"$1" "cd $lachesis_dir; [ -f build/lachesis ] || make vendor build; sudo mv /mnt/data/$go_lachesis_service_file /lib/systemd/system/;
   sudo systemctl daemon-reload && ( sudo systemctl stop $go_lachesis 2>/dev/null; sudo systemctl start $go_lachesis; )" | sed "s/^/[${nodes[$1]}] /";
 
   # EVM
   declare -r go_evm='go-evm';
-  declare -r evm_dir='/home/ubuntu/go/src/github.com/Fantom-foundation/'"$go_evm";
+  declare -r evm_dir='/home/ubuntu/go/src/github.com/SamuelMarks/'"$go_evm";
   env -i PATH="$PATH" BUILD_DIR="$evm_dir/build" NODE="$1" NODE_ADDR="${nodes[$1]}" envsubst < "$parent_dir"/go-evm.tpl.service > "$parent_dir"/"$evm_service_file"."$1";
   rsync -avz "$parent_dir"/"$evm_service_file"."$1" testnet"$1":/mnt/data/"$evm_service_file";
   ssh testnet"$1" "cd $evm_dir; [ -f evm ] || make vendor build; sudo mv /mnt/data/$evm_service_file /lib/systemd/system/; sudo systemctl daemon-reload && ( sudo systemctl stop $go_evm 2>/dev/null; sudo systemctl start $go_evm )" | sed "s/^/[${nodes[$1]}] /";
--- scripts/testnet.bash
+++ scripts/testnet.bash
@@ -20,17 +20,17 @@ declare -r node_addr="${node_addr:-127.0.0.1}"
 "$DIR/docker/install_deps.bash"
 
 # Use -tags="netgo multi" in bgo build below to build multu lachesis version for testing
-declare args="-X github.com/Fantom-foundation/go-lachesis/src/version.GitCommit=$(git rev-parse HEAD)"
+declare args="-X github.com/SamuelMarks/dag1/src/version.GitCommit=$(git rev-parse HEAD)"
 if [ "$TARGET_OS" == "linux" ]; then
   args="$args -linkmode external -extldflags -static -s -w"
 fi
 env GOOS="$TARGET_OS" GOARCH=amd64 go build -tags="netgo" -ldflags "$args" -o lachesis_"$TARGET_OS" "$parent_dir/cmd/lachesis/$entry.go" || exit 1
 
-# Create peers.json and lachesis_data_dir if needed
-if [ ! -d "$DATAL_DIR/lachesis_data_dir" ]; then
-    echo "$DATAL_DIR/lachesis_data_dir is not found; can not run"
+# Create peers.json and dag1_data_dir if needed
+if [ ! -d "$DATAL_DIR/dag1_data_dir" ]; then
+    echo "$DATAL_DIR/dag1_data_dir is not found; can not run"
     exit 2
 fi
 
-GOMAXPROCS=$(($logicalCpuCount - 1)) "$BUILD_DIR/lachesis_$TARGET_OS" run --datadir "$DATAL_DIR/lachesis_data_dir/$node" --store --listen="$node_addr":12000 --log=warn --heartbeat=5s -p "$node_addr":9000 -s "$node_addr":9090 --syslog
+GOMAXPROCS=$(($logicalCpuCount - 1)) "$BUILD_DIR/lachesis_$TARGET_OS" run --datadir "$DATAL_DIR/dag1_data_dir/$node" --store --listen="$node_addr":12000 --log=warn --heartbeat=5s -p "$node_addr":9000 -s "$node_addr":9090 --syslog
 
