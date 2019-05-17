36ec8de44f3a594dafa8d36fd0f9fb55e6c41100
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-05-17 15:01:31 +1000

    autogenerate tesnet configuration based on the number of nodes listed

--- scripts/testnet-master.bash
+++ scripts/testnet-master.bash
@@ -2,11 +2,23 @@
 
 set -euo pipefail
 
+#
+# The script to bootstrap Fantom Foundation testnet
+#
+# The nodes array contains the list of addresses of nodes used for inter node communications
+# currently they are grey addreses of AWS instances ised in deployment
+# To access these instances testnet0, testnet1, ..., testnet$n ($n number of nodes) aliases
+# should be specified in ~/.ssh/config file and the machine where this script is running
+# should be abble to access these instances whithout specifying additional parameters on command line
+# (i.e. all connnection parameters should be in ~/.ssh/config file)
+#
+# batch-ethkey is required, see how to install it here: https://github.com/SamuelMarks/batch-ethkey
+#
+declare -ar nodes=('172.31.6.65' '172.31.8.33' '172.31.5.183' '172.31.3.125' '172.31.1.84');
+declare -ri port=12000
+
 declare -r DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 declare -r parent_dir="${DIR%/*}";
-
-declare -ar nodes=('172.31.6.65' '172.31.8.33' '172.31.5.183' '172.31.3.125' '172.31.1.84');
-
 declare -r go_lachesis_service_file='dag1.service';
 declare -r evm_service_file='go-evm.service'
 declare -r go_lachesis_branch='master'
@@ -26,12 +38,13 @@ function deploy() {
   
   # Config
   rsync -avz "$parent_dir"/scripts/testnet.bash testnet"$1":/home/ubuntu/go/src/github.com/SamuelMarks/dag1/scripts/;
-  rsync -avz "$parent_dir"/testnet/dag1_data_dir testnet"$1":/mnt/data/ --rsync-path='sudo rsync';
-  ssh testnet"$1" "mkdir -p /mnt/data/evm/eth; rm -rf ~/.evm ; ln -sf /mnt/data/evm ~/.evm";
-  rsync -avz "$parent_dir"/testnet/eth/"$1"/ testnet"$1":/mnt/data/evm/eth --rsync-path='sudo rsync';
+  ssh testnet"$1" "sudo rm -rf /mnt/data/dag1_data_dir /mnt/data/evm ; mkdir -p /mnt/data/dag1_data_dir /mnt/data/evm; rm -rf ~/.evm ; ln -sf /mnt/data/evm ~/.evm";
+  rsync -avz "$parent_dir"/testnet/"$1"/ testnet"$1":/mnt/data/dag1_data_dir --rsync-path='sudo rsync';
+  ssh testnet"$1" "ln -sf /mnt/data/dag1_data_dir/eth  /mnt/data/evm/eth";
   #  ssh testnet"$1" "mkdir -p ~/.evm/eth" | sed "s/^/[${nodes[$1]}] /";
   ssh testnet"$1" "mv ~/.evm/eth/evml.toml ~/.evm/; ";
   rsync -avz "$parent_dir"/testnet/genesis.json testnet"$1":/home/ubuntu/.evm/eth/;
+  rsync -avz "$parent_dir"/testnet/peers.json testnet"$1":/mnt/data/dag1_data_dir/;
 
   # Lachesis
   env -i PATH="$PATH" DATAL_DIR='/mnt/data' BUILD_DIR="$lachesis_dir/build" NODE="$1" NODE_ADDR="${nodes[$1]}" envsubst < "$parent_dir"/dag1.tpl.service > "$parent_dir"/"$go_lachesis_service_file"."$1";
@@ -48,7 +61,7 @@ function deploy() {
 function init() {
     ssh testnet"$1" 'if ! $(df -h | grep -q /mnt/data); then sudo rm -rfv /mnt/data; sleep 1s && ( export ssd=`lsblk | grep 1.8T | sed -e "s/\s.*$//"` ; echo $ssd ; sudo mkfs -t ext4 /dev/$ssd; sudo mkdir /mnt/data; sudo mount /dev/$ssd /mnt/data; sudo chown -R ubuntu:ubuntu /mnt/data/ ); fi';
     # slash down existing badger db for dag1; a temporary solution for consensus crash on startup with existing database
-    ssh testnet"$1" "if [ -d /mnt/data/dag1_data_dir/$1/badger_db ]; then sudo rm -rf /mnt/data/dag1_data_dir/$1/badger_db; fi";
+    ssh testnet"$1" "if [ -d /mnt/data/dag1_data_dir/badger_db ]; then sudo rm -rf /mnt/data/dag1_data_dir/badger_db; fi";
     ssh testnet"$1" 'sudo apt install -y protobuf-compiler golang-goprotobuf-dev && sudo ln -sf /usr/share/zoneinfo/Australia/Sydney /etc/localtime && date';
     ssh testnet"$1" "if [ ! -f /etc/logrotate-fantom.conf ]; then cat <<EOT | sudo tee /etc/logrotate-fantom.conf
 /var/log/syslog
@@ -63,6 +76,12 @@ function init() {
 EOT
 sudo cp /etc/cron.daily/logrotate /etc/cron.hourly/ && sudo sed -i 's/logrotate.conf/logrotate-fantom.conf/' /etc/cron.hourly/logrotate && sudo service rsyslog rotate ; fi"
 }
+
+
+declare -ra hosts=("${nodes[@]/#/-host }")
+
+rm -rf testnet
+batch-ethkey -dir ./testnet -n ${#nodes[@]} -evm ${hosts[@]/%/:$port} -network 127.0.0.1
 
 # use these commands for debug ona single node
 #init "0" && deploy "0"
